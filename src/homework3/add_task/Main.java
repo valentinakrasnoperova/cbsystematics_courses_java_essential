906383a2ec1636ec3d1706f7c0b23090974b7f1e
valentinakrasnoperova <kresnoperova.valentina2003@gmail.com>
2021-04-01 12:46:50 +0300

    MASTER-3: added homework3 - inheritance

--- src/homework3/add_task/Main.java
+++ src/homework3/add_task/Main.java
@@ -2,7 +2,7 @@ package homework3.add_task;
 
 public class Main {
     public static void main(String[] args) {
-        ColoredPrinter coloredPrinter = new ColoredPrinter("hello");
+        Printer coloredPrinter = new ColoredPrinter("hello");
         coloredPrinter.print("hello");
     }
 
